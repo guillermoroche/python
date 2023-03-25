5c159b8684d237b08ef69bf0c0572ae83cc1568b
guillermoroche <72481479+guillermoroche@users.noreply.github.com>
2023-03-25 22:26:21 +0100

    Initial Commit

---
+++ grafico.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math as math
+
+xpoints = np.linspace(-25,25,100000)
+ypoints = xpoints
+ypoints2 = xpoints**2
+
+print(xpoints)
+print(ypoints)
+plt.plot(xpoints, ypoints, ypoints2)
+#plt.title("Titulo de prueba")
+
+#plt.grid(color='r', linestyle='-', linewidth=0.5)
+plt.show()
